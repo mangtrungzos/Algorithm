23a9d0e8aa4d3f94d84e5098c3698ca154c8a7d2
mangtrungzos <sangv906@gmail.com>
2024-07-22 22:34:22 +0700

    22/7

--- AllowOneFunctionCall.js
+++ AllowOneFunctionCall.js
@@ -32,3 +32,15 @@ var once = function(fn) {
 
 // output expected
 // [{"calls:1", "value": 6}]
+
+// 1. Hàm fn nhận ba tham số (a, b, c) và trả về tổng của ba tham số này: a + b + c
+// 2. `hasBeenCalled` là một biến cờ, ban đầu được gán giá trị `false`, dùng để theo dõi xem hàm `fn` đã được gọi hay chưa.
+// 3. `result` lưu trữ kết quả của lần gọi đầu tiên
+// 4. Trong hàm trả về:
+// 5. Nếu `hasBeenCalled` là `false`, tức là hàm `fn` chưa được gọi 
+// 6. Ở đây `fn(...args)`: args: [1,2,3] -> ...args : fn(1,2,3)
+// 7. Hàm `fn` sẽ được gọi với các tham số truyền vào và kết quả sẽ được lưu vào `result`
+// 8. Sau đó, `hasBeenCalled` được gán là `true` và kết quả được trả về.
+
+// 9. Khi gọi lại `once(fn)(2, 3, 6)`, do `hasBeenCalled` đã là `true`
+// 10. hàm `fn` không được thực thi nữa và hàm chỉ trả về `undefined`.
